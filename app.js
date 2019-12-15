645ca5d4ac37942d37a44e4d0337ff568b7cae65
ridho <ridhopratamavd@gmail.com>
2019-12-15 17:51:32 +0700

    refactor double quotes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const createError = require('http-errors')
+const express = require('express');
+const mongoose = require('mongoose');
+const createError = require('http-errors');
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 mongoose
   .connect('mongodb://localhost/tddDB', {
@@ -16,26 +16,26 @@ mongoose
     console.log('Connected to MongoDB at mongodb://localhost/tddDB...')
   )
   .catch(err => {
-    console.log('Failed to connect to MongoDB...', err)
+    console.log('Failed to connect to MongoDB...', err);
     process.exit()
-  })
+  });
 
-const usersRouter = require('./routes/user')
+const usersRouter = require('./routes/user');
 
-app.use('/api/users', usersRouter)
+app.use('/api/users', usersRouter);
 
 app.use((req, res, next) => {
   next(createError(404))
-})
+});
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message
+  res.locals.message = err.message;
 
   // render the error page
-  res.status(err.status || 500)
+  res.status(err.status || 500);
   res.send(err)
-})
+});
 
-module.exports = app
+module.exports = app;
