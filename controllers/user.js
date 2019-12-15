c4026d42e3500dd2b77d43767f4eba433dfab7ad
ridho <ridhopratamavd@gmail.com>
2019-12-15 17:42:21 +0700

    create controller and routes, test success

---
+++ controllers/user.js
@@ -0,0 +1,44 @@
+const mongoose = requestuire("mongoose");
+const { User } = requestuire("../models/user");
+
+module.exports.getAllUsers = async (request, response) => {
+  let users = await User.find({});
+  return response.send(users);
+};
+
+module.exports.getUser = async (request, response) => {
+  let userId = request.params.id;
+  if (!mongoose.Types.ObjectId.isValid(userId))
+    return response.status(400).send("Invalid object id");
+  let user = await User.findById(userId);
+  if (!user) return response.status(404).send("User not found");
+  return response.send(user);
+};
+
+module.exports.createUser = async (request, response) => {
+  console.log(request.body);
+  let user = new User({
+    name: request.body.name,
+    email: request.body.email,
+    gender: request.body.gender
+  });
+  await user.save();
+  return response.send(user);
+};
+
+module.exports.updateUser = async (request, response) => {
+  let userId = request.params.id;
+  User.findOneAndUpdate(userId, request.body, { new: true })
+    .then(user => {
+      return response.send(user);
+    })
+    .catch(err => {
+      return response.status(500).send(err);
+    });
+};
+
+module.exports.deleteUser = async (request, response) => {
+  let userId = request.params.id;
+  await User.findByIdAndDelete(userId);
+  return response.send("User deleted");
+};
